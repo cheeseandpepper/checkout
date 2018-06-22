dd5ea7cfe87013d329ae979836a572d546e0b12e
Mike <cheeseandpepper@gmail.com>
2018-06-22 00:05:00 -0700

    removed debug messages and bumped version

--- lib/checkout/runner.rb
+++ lib/checkout/runner.rb
@@ -6,21 +6,16 @@ module Checkout
 
     def initialize(max)
       if max.nil?
-        puts "max is 10"
         @max ||= 10
       else
-        puts "max is #{max}"
         @max ||= max.to_i
       end
       run!
     end
 
     def run!
-      puts "asking"
       ask_for_selection
-      puts 'mapping selection'
       map_selection_to_branch
-      puts 'checking out'
       checkout
     end
 
@@ -34,10 +29,7 @@ module Checkout
     end
 
     def map_selection_to_branch
-      puts "selection is #{selection}"
       @branch ||= branch_choice_object[selection.to_i]
-      puts "branch is #{@branch}"
-      puts "branch choice object is #{branch_choice_object}"
     end
 
     def branches
@@ -53,12 +45,7 @@ module Checkout
 
     def display_branches
       branch_choice_object.each do |k, v|
-        puts "k is #{k}"
-        puts "v is #{v}"
-        puts "k dot size is #{k.to_s.size}"
-        puts "branch is #{branch}"
         dashes = "-" * (5 - k.to_s.size)
-        puts "#{k.to_s.colorize(:yellow)} #{dashes} #{v.colorize(:green)}"
       end
     end
 
